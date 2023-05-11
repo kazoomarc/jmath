d676b3b42e4d752db1472210c21f91f2f426afe9
Joel Mwala <107686531+kazoomarc@users.noreply.github.com>
2023-05-11 13:51:11 +0100

    correct NULL to required

--- Inputtest.java
+++ Inputtest.java
@@ -1,3 +1,10 @@
+import tech.joelmwala.util.*;
+
 public class Inputtest {
+    public static void main(String[] args) {
+        int num = In12put.getInt("Enter num: ");
+        System.out.println(num);
+
+    }
 
 }
